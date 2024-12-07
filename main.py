99bbe7556200f62e2c8aeb94a76b4d6efc515163
Akash Goyal <139630240+1223akash@users.noreply.github.com>
2024-12-07 15:49:11 +0530

    Add comments in main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# Step 1
 import pandas as pd
 
-# Step 2
 df = pd.read_csv('bestsellers.csv')
 
-# Step 3
 # Get the first 5 rows of the spreadsheet
 print(df.head())
 
@@ -17,19 +14,18 @@ print(df.columns)
 # Get summary statistics for each column
 print(df.describe())
 
-# Step 4
+# Remove the duplicates
 df.drop_duplicates(inplace=True)
 
 df["Price"] = df["Price"].astype(float)
 
 df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
 
-# Step 5
 author_counts = df['Author'].value_counts()
 print(author_counts)
 
 avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
 print(avg_rating_by_genre)
 
-# Step 6
+# Generate the top 10 authors in seperate file
 author_counts.head(10).to_csv("top_authors.csv")
